881b0bceaf23263655997b73461e0382d07107ff
Troy Grossi <ttroygrossit@gmail.com>
2021-08-31 16:54:09 -0700

    comment

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const routes = require("./controllers/index.js");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const exphbs = require("express-handlebars");
 
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require("dotenv").config();
+
 const sess = {
   secret: "secret",
   cookie: {},
@@ -16,12 +19,8 @@ const sess = {
     db: sequelize,
   }),
 };
+
 app.use(session(sess));
-
-// handlebars middleware
-const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
